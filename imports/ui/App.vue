b754b30c7569f72fdc539206ad057ce1cb187624
Mrau Hu <mrauhu@yandex.ru>
2020-06-08 01:13:47 +0300

    Created `platformLog()` function and logging for Vue lifecycle hooks.

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -5,10 +5,40 @@
 </template>
 
 <script>
+  import { Meteor } from 'meteor/meteor';
+
+  function platformLog(...args) {
+    const platform = Meteor.isServer
+      ? 'server'
+      : 'client';
+    console.log(platform, ...args);
+  }
+
   export default {
     name: 'App',
     metaInfo: () => ({
       title: `vue-meta and Meteor+Vue SSR`,
     }),
+    serverPrefetch() {
+      platformLog('serverPrefetch()');
+    },
+    beforeCreate() {
+      platformLog('beforeCreate()');
+    },
+    created() {
+      platformLog('created()');
+    },
+    beforeMount() {
+      platformLog('beforeMount()');
+    },
+    mounted() {
+      platformLog('mounted()');
+    },
+    beforeDestroy() {
+      platformLog('beforeDestroy()');
+    },
+    destroyed() {
+      platformLog('destroyed()')
+    },
   };
 </script>
